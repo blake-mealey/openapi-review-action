89de89ebad99a416e51fefd91a87c1ea1d996ba2
Blake Mealey <blakemealey@gmail.com>
2020-09-10 17:51:07 -0600

    added first real implementation

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const widdershins = require('widdershins');
+const fs = require('fs');
 
 try {
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput('time', time);
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  const specPath = core.getInput('spec-path');
+
+  console.log(`Processing spec: ${specPath}`);
+
+  const spec = fs.readFileSync(specPath, { encoding: 'utf-8' });
+
+  const docs = widdershins.convert(spec, {});
+
+  console.log(docs);
 } catch (error) {
   core.setFailed(error.message);
 }
